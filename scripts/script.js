69edfe4c9fa142ce350b89c0681a900153416d3d
Anup Shetty <anuprshetty@gmail.com>
2021-06-22 12:23:59 +0530

    JS alert and prompt

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 console.log("Welcome to Home page");
+// Single-line comment
+/* 
+    Multi-line comment 
+*/
+alert("Nice to meet you");
+var myName = prompt("What is your name?");
+console.log("Your name: " + myName);
