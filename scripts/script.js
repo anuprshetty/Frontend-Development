44dbc2f571f1c3b8896f7c3d3a26ea8d8b73b112
Anup Shetty <anuprshetty@gmail.com>
2021-06-24 11:18:59 +0530

    JS 3 types of Popups, Global and Local scopes

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,25 @@ console.log("Welcome to Home page");
 /* 
     Multi-line comment 
 */
+
+// 3 types of popups in JS.
 alert("Nice to meet you");
+confirm("Do you want to continue?");
 var myName = prompt("What is your name?");
+
 console.log("Your name: " + myName);
+
+var myString = "my string";
+var myNumber = 5;
+var myBoolean = true;
+
+var myVar = "GLOBAL";
+
+function funScope(){
+    console.log(myVar); // undefined  --> Because funScope - myVar variable is considered. (Local scope)
+    var myVar = "LOCAL";
+    console.log(myVar); // LOCAL
+}
+console.log(myVar); // GLOBAL
+funScope();
+console.log(myVar); // GLOBAL
